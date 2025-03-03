0e1f54d11879e5519be6ac0dc6d26da88622c5b7
loevc <liuxinsisi71@gmail.com>
2025-03-03 20:47:47 +0800

    feat(): init springboot pro []

---
+++ src/main/java/org/loevc/velora/VeloraApplication.java
@@ -0,0 +1,13 @@
+package org.loevc.velora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeloraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/loevc/velora/VeloraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.loevc.velora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
