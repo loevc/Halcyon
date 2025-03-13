9ebe997e052ee36bb7d019e017cd331eb293444d
loevc <liuxinsisi71@gmail.com>
2025-03-14 07:51:22 +0800

    feat(v0.0.2): modify project name [Halcyon]

--- src/main/java/org/loevc/halcyon/HalcyonApplication.java
+++ src/main/java/org/loevc/halcyon/HalcyonApplication.java
@@ -1,13 +1,13 @@
-package org.loevc.velora;
+package org.loevc.halcyon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VeloraApplication {
+public class HalcyonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VeloraApplication.class, args);
+        SpringApplication.run(HalcyonApplication.class, args);
     }
 
 }
