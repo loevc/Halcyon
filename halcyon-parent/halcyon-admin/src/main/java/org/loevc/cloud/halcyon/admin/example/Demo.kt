76f5c746e904f1f9ff78aa2ca7c83d9f75e812fc
loevc <33386034+loevc@users.noreply.github.com>
2026-01-14 17:39:53 +0800

    feat(v0.0.7): add kotlin support [Halcyon]

---
+++ halcyon-parent/halcyon-admin/src/main/java/org/loevc/cloud/halcyon/admin/example/Demo.kt
@@ -0,0 +1,8 @@
+package org.loevc.cloud.halcyon.admin.example
+
+object Demo {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("damn ")
+    }
+}
---
+++ halcyon-parent/halcyon-common/src/main/kotlin/org/loevc/cloud/halcyon/common/entity/Demo.kt
@@ -0,0 +1,5 @@
+package org.loevc.cloud.halcyon.common.entity
+
+fun main(){
+    println("fucking")
+}
