c5d4d07a7c833b9bf775f53ffcae0b66211e6cc1
loevc <33386034+loevc@users.noreply.github.com>
2026-07-18 13:06:22 +0800

    feat(v0.0.8): algo record leetcode 709 update  [Halcyon]

--- src/main/java/org/loevc/halcyon/leetcode/Q709.java
+++ src/main/java/org/loevc/halcyon/leetcode/Q709.java
@@ -6,6 +6,7 @@ public class Q709 {
      * 问题： 能不能通过位运算来实现呢？
      * 我记得 大小写字母是有特殊值的，
      * 想想哈，其实 大写 + 32 = 小写，也就是 2^5, 那么就是 10_0000
+     * 不对哈，题目中是可打印字符，可打印字符不仅仅包含 大小写字母，还有数字和符号，所以不能直接使用位运算来实现
      */
     class Solution {
         public String toLowerCase(String s) {
@@ -13,7 +14,7 @@ public class Q709 {
             byte[] bytes = s.getBytes();
             for (byte b : bytes) {
                 if (b >= 'A' && b <= 'Z')
-                    b += 32;
+                    b |= 32;
                 sb.append((char) b);
             }
             return sb.toString();
