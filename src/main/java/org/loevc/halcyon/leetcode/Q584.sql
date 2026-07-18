b58283391644159fffded5924cf936fb6e60725c
loevc <33386034+loevc@users.noreply.github.com>
2026-07-18 12:56:45 +0800

    feat(v0.0.8): algo record leetcode 1757  [Halcyon]

--- src/main/java/org/loevc/halcyon/leetcode/Q584.sql
+++ src/main/java/org/loevc/halcyon/leetcode/Q584.sql
@@ -1,1 +1,5 @@
 select name from Customer where referee_id != 2 or referee_id is null;
+
+-- 在 SQL 中，NULL 表示“未知”或“缺失”，任何与 NULL 进行直接比较（包括 =、!=、<>、> 等）的结果都不是 TRUE，而是 UNKNOWN。
+--
+-- WHERE 子句只接受结果为 TRUE 的行，UNKNOWN 和 FALSE 都会被过滤掉。
