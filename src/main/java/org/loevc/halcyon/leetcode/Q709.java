236efdf2e30fd59b688873af29de3aa13671856c
loevc <33386034+loevc@users.noreply.github.com>
2026-07-18 13:02:56 +0800

    feat(v0.0.8): algo record leetcode 709  [Halcyon]

---
+++ src/main/java/org/loevc/halcyon/leetcode/Q709.java
@@ -0,0 +1,22 @@
+package org.loevc.halcyon.leetcode;
+
+public class Q709 {
+
+    /**
+     * 问题： 能不能通过位运算来实现呢？
+     * 我记得 大小写字母是有特殊值的，
+     * 想想哈，其实 大写 + 32 = 小写，也就是 2^5, 那么就是 10_0000
+     */
+    class Solution {
+        public String toLowerCase(String s) {
+            StringBuilder sb = new StringBuilder();
+            byte[] bytes = s.getBytes();
+            for (byte b : bytes) {
+                if (b >= 'A' && b <= 'Z')
+                    b += 32;
+                sb.append((char) b);
+            }
+            return sb.toString();
+        }
+    }
+}
